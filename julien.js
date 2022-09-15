3a2d6131435c526a5f3cba469d484c2fc56175a8
Angel Hmeli <angel.hmeli@edu.devinci.fr>
2022-09-15 18:01:03 +0200

    tete tourne

--- julien.js
+++ julien.js
@@ -1,15 +1,54 @@
-const tabs = document.querySelectorAll('[data-tab-target]')               //select le truc à cliquer
-const tabContents = document.querySelectorAll('[data-tab-content]')       //select le contenu du truc à cliquer
+const tabs = document.querySelectorAll('[data-tab-target]')               
+const tabContents = document.querySelectorAll('[data-tab-content]')       
 
-tabs.forEach(tab => {                                                       // on parcoure les elemens tab
+tabs.forEach(tab => {                                                      
   tab.addEventListener('click', () => {                                      
-    tab.classList.remove('active')                                            //au click  ---> on enlève active du truc à cliquer
-    const target = document.querySelector(tab.getAttribute('data-tab-target'))   //---> nvlle const qui prend l'id de la target (le contenu donc)
+    tab.classList.remove('active')                                            
+    const target = document.querySelector(tab.getAttribute('data-tab-target'))   
 
-    tabContents.forEach(tabContent => {                                         //---> on parcoure les contents
-      tabContent.classList.remove('active')                                     //---> on enlève la class active
+    tabContents.forEach(tabContent => {                                       
+      tabContent.classList.remove('active')                              
   })
-    tab.classList.add('active')                                                //enfin on rajoute la class active au truc cliqué...
-    target.classList.add('active')                                             // ... et à son contenu
+    tab.classList.add('active')                                            
+    target.classList.add('active')                                       
   })
 })
+
+let tourn = document.querySelector(".tofja") 
+if(tourn != null){
+tourn.addEventListener("mouseover", mOver, false) 
+tourn.addEventListener("mouseout", mOut, false) 
+
+function mOver() {
+   tourn.setAttribute("style", "transform: scale(1.5) rotate(1.0turn); transition: all .5s ease-in-out; ")
+}
+function mOut() {  
+   tourn.setAttribute("style", "transform: scale(1.0) ; transition: all .2s ease-in-out; ")
+}
+}
+
+let tournb = document.querySelector(".tofjb") 
+if(tournb != null){
+tournb.addEventListener("mouseover", mOver, false) 
+tournb.addEventListener("mouseout", mOut, false) 
+
+function mOver() {
+   tournb.setAttribute("style", "transform: scale(1.5) rotate(1.0turn); transition: all .5s ease-in-out; ")
+}
+function mOut() {  
+   tournb.setAttribute("style", "transform: scale(1.0) ; transition: all .2s ease-in-out; ")
+}
+}
+
+let tournc = document.querySelector(".tofjc") 
+if(tournc != null){
+tournc.addEventListener("mouseover", mOver, false) 
+tournc.addEventListener("mouseout", mOut, false) 
+
+function mOver() {
+   tournc.setAttribute("style", "transform: scale(1.5) rotate(1.0turn); transition: all .5s ease-in-out; ")
+}
+function mOut() {  
+   tournc.setAttribute("style", "transform: scale(1.0) ; transition: all .2s ease-in-out; ")
+}
+}
